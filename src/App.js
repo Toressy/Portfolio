81e1e61b6c255b2378d363746bde9d487fd4f251
Toressy <titarenkoviktoria16@gmail.com>
2025-06-17 12:27:15 +0100

    .

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@ import React from "react";
 import "./App.scss";
 import Main from "./containers/Main";
 
+console.log("GitHub Username:", process.env.REACT_APP_GITHUB_USERNAME);
+console.log("Using GitHub Data:", process.env.REACT_APP_USE_GITHUB_DATA);
+
 function App() {
   return (
     <div>
